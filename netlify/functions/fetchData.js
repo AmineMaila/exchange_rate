6e79700fa31f36c85fa927317ab777d6fc33fc43
AmineMaila <aminemaila18@gmail.com>
2025-04-27 21:22:26 +0100

    changed axios

--- netlify/functions/fetchData.js
+++ netlify/functions/fetchData.js
@@ -1,10 +1,10 @@
-import axios from "axios"
+const axios = require("axios");
 
-export const handler = (event) => {
+exports.handler = async (event) => {
   const API_KEY = process.env.EXCHANGE_API_KEY;
   const { query } = event.queryStringParameters || {};
 
-	if (!query) {
+  if (!query) {
     return {
       statusCode: 400,
       headers: { "Content-Type": "application/json" },
@@ -12,26 +12,26 @@ export const handler = (event) => {
     };
   }
 
-	return axios.get(`https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${query}`)
-		.then((response) => ({
-			statusCode: response.status,
-			headers: {
-				"Content-Type": "application/json",
-				"Access-Control-Allow-Origin": "*"
-			},
-			body: JSON.stringify(response.data)
-		}))
-		.catch((error) => {
-			const statusCode = error.response?.status || 500
-			const errorMsg = error.response?.data?.message || 'Failed to fetch data'
-
-			return {
-				statusCode: statusCode,
-				headers: {
-					"Content-Type": "application/json",
-					"Access-Control-Allow-Origin": "*"
-				},
-				body: JSON.stringify({ error: errorMsg }),
-			}
-		})
-}
+  try {
+    const response = await axios.get(`https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${query}`);
+    return {
+      statusCode: response.status,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify(response.data),
+    };
+  } catch (error) {
+    const statusCode = error.response?.status || 500;
+    const errorMsg = error.response?.data?.message || "Failed to fetch data";
+    return {
+      statusCode: statusCode,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({ error: errorMsg }),
+    };
+  }
+};
